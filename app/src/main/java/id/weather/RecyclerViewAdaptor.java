88ef248138bdc1c9499b4ddcc0e516fad8bd68bd
Ofsouzap <ofsouzap@gmail.com>
2023-05-16 14:29:26 +0100

    Create RecyclerView classes

---
+++ app/src/main/java/id/weather/RecyclerViewAdaptor.java
@@ -0,0 +1,26 @@
+package id.weather;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdaptor extends RecyclerView.Adapter<RecyclerViewViewHolder> {
+
+    @NonNull
+    @Override
+    public RecyclerViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null; // TODO
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewViewHolder holder, int position) {
+        // TODO
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0; // TODO
+    }
+
+}
---
+++ app/src/main/java/id/weather/RecyclerViewViewHolder.java
@@ -0,0 +1,17 @@
+package id.weather;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerViewViewHolder(@NonNull View itemView) {
+        super(itemView);
+        // TODO
+    }
+
+    // TODO
+
+}

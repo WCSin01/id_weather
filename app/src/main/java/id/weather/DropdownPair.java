d50beb3efea4b4a00ccec79c8524952aefca6ec6
SkyBatchelor <skybatchelor@yahoo.com>
2023-05-17 18:19:37 +0100

    moved code into a class

---
+++ app/src/main/java/id/weather/DropdownPair.java
@@ -0,0 +1,71 @@
+package id.weather;
+
+import android.animation.ValueAnimator;
+import android.view.ViewGroup;
+
+public class DropdownPair {
+    private final ViewGroup layout1, layout2;
+    private final int closedHeight;
+    private boolean layout1Open;
+
+    /**
+     * Pairs the given layouts as dropdowns which will open/close when the other is closed/opened.
+     *
+     * @param openLayout the layout which is initially open
+     * @param closedLayout the layout which is initially closed
+     */
+    public DropdownPair(ViewGroup openLayout, ViewGroup closedLayout) {
+        layout1 = openLayout;
+        layout2 = closedLayout;
+
+        // need to set default closed height now in case dropdowns are toggled during animation
+        // don't need open height as well because this is encoded implicitly since they always
+        // move in unison
+        closedHeight = closedLayout.getLayoutParams().height;
+
+        layout1Open = true;
+    }
+
+    /**
+     * Toggles the state of the paired dropdowns.
+     *
+     * @param animDuration the duration of the open/close animation in milliseconds
+     */
+    public void toggle(int animDuration) {
+        // find which layout is open and which is closed
+        final ViewGroup openLayout, closedLayout;
+        if (layout1Open) {
+            openLayout = layout1;
+            closedLayout = layout2;
+        }
+        else {
+            openLayout = layout2;
+            closedLayout = layout1;
+        }
+
+        // find current heights of the layouts to ensure smooth animation if toggled during
+        // previous animation
+        int currentOpenHeight = openLayout.getLayoutParams().height;
+        int currentClosedHeight = closedLayout.getLayoutParams().height;
+
+        // create animator for smooth transition
+        ValueAnimator anim = ValueAnimator.ofInt(currentOpenHeight, closedHeight);
+        anim.addUpdateListener(valueAnim -> {
+            int val = (int)valueAnim.getAnimatedValue();
+
+            ViewGroup.LayoutParams openParams = openLayout.getLayoutParams();
+            openParams.height = val;
+            openLayout.setLayoutParams(openParams);
+
+            ViewGroup.LayoutParams closedParams = closedLayout.getLayoutParams();
+            closedParams.height = currentClosedHeight + currentOpenHeight - val;
+            System.out.println(val);
+            closedLayout.setLayoutParams(closedParams);
+        });
+
+        anim.setDuration(animDuration).start();
+
+        // swap open and closed layouts
+        layout1Open = !layout1Open;
+    }
+}
--- app/src/main/java/id/weather/MainActivity.java
+++ app/src/main/java/id/weather/MainActivity.java
@@ -1,6 +1,5 @@
 package id.weather;
 
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,38 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // todo: to pass into WeatherApi what to update
 
-        // TODO: refactor into somewhere more sensible than the main function
         ViewGroup dailyLayout = findViewById(R.id.dailyLayout);
         ViewGroup weeklyLayout = findViewById(R.id.weeklyLayout);
 
-        // default values for layout heights - get from design to avoid hardcoding
-        int openHeight = dailyLayout.getLayoutParams().height;
-        int closedHeight = weeklyLayout.getLayoutParams().height;
-
-        // create animators to open/close dropdowns - each one handles both dropdowns
-        // to avoid code repetition
-        ValueAnimator.AnimatorUpdateListener listener = valueAnim -> {
-            int val = (int)valueAnim.getAnimatedValue();
-
-            ViewGroup.LayoutParams dailyParams = dailyLayout.getLayoutParams();
-            dailyParams.height = val;
-            dailyLayout.setLayoutParams(dailyParams);
-
-            ViewGroup.LayoutParams weeklyParams = weeklyLayout.getLayoutParams();
-            weeklyParams.height = openHeight + closedHeight - val;
-            weeklyLayout.setLayoutParams(weeklyParams);
-        };
-
-        ValueAnimator openDailyAnim = ValueAnimator.ofInt(closedHeight, openHeight);
-        openDailyAnim.addUpdateListener(listener);
-        ValueAnimator openWeeklyAnim = ValueAnimator.ofInt(openHeight, closedHeight);
-        openWeeklyAnim.addUpdateListener(listener);
+        DropdownPair dropdowns = new DropdownPair(dailyLayout, weeklyLayout);
 
         // set listeners on buttons to run the animation when pressed
         Button dailyButton = findViewById(R.id.dailyButton);
-        dailyButton.setOnClickListener(v -> openDailyAnim.setDuration(300).start());
+        dailyButton.setOnClickListener(v -> dropdowns.toggle(300));
 
         Button weeklyButton = findViewById(R.id.weeklyButton);
-        weeklyButton.setOnClickListener(v -> openWeeklyAnim.setDuration(300).start());
+        weeklyButton.setOnClickListener(v -> dropdowns.toggle(300));
     }
 }

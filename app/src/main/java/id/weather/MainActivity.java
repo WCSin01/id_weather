87eb300f1578603d0a17050b14c4fd186f9cd63f
SkyBatchelor <skybatchelor@yahoo.com>
2023-05-17 15:50:16 +0100

    rudimentary functional code - needs refactoring

--- app/src/main/java/id/weather/MainActivity.java
+++ app/src/main/java/id/weather/MainActivity.java
@@ -1,18 +1,52 @@
 package id.weather;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    // todo: to pass into WeatherApi what to update
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // todo: to pass into WeatherApi what to update
+
+        // TODO: refactor into somewhere more sensible than the main function
+        ViewGroup dailyLayout = findViewById(R.id.dailyLayout);
+        ViewGroup weeklyLayout = findViewById(R.id.weeklyLayout);
+
+        // default values for layout heights - get from design to avoid hardcoding
+        int openHeight = dailyLayout.getLayoutParams().height;
+        int closedHeight = weeklyLayout.getLayoutParams().height;
+
+        // create animators to open/close dropdowns - each one handles both dropdowns
+        // to avoid code repetition
+        ValueAnimator.AnimatorUpdateListener listener = valueAnim -> {
+            int val = (int)valueAnim.getAnimatedValue();
+
+            ViewGroup.LayoutParams dailyParams = dailyLayout.getLayoutParams();
+            dailyParams.height = val;
+            dailyLayout.setLayoutParams(dailyParams);
+
+            ViewGroup.LayoutParams weeklyParams = weeklyLayout.getLayoutParams();
+            weeklyParams.height = openHeight + closedHeight - val;
+            weeklyLayout.setLayoutParams(weeklyParams);
+        };
+
+        ValueAnimator openDailyAnim = ValueAnimator.ofInt(closedHeight, openHeight);
+        openDailyAnim.addUpdateListener(listener);
+        ValueAnimator openWeeklyAnim = ValueAnimator.ofInt(openHeight, closedHeight);
+        openWeeklyAnim.addUpdateListener(listener);
+
+        // set listeners on buttons to run the animation when pressed
+        Button dailyButton = findViewById(R.id.dailyButton);
+        dailyButton.setOnClickListener(v -> openDailyAnim.setDuration(300).start());
+
+        Button weeklyButton = findViewById(R.id.weeklyButton);
+        weeklyButton.setOnClickListener(v -> openWeeklyAnim.setDuration(300).start());
+    }
 }

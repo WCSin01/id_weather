3a7bf2bbd7ad9e917816d5191ca36715c442214b
Ofsouzap <ofsouzap@gmail.com>
2023-05-18 14:49:40 +0100

    RecyclerView system with basic example items

--- app/src/main/java/id/weather/MainActivity.java
+++ app/src/main/java/id/weather/MainActivity.java
@@ -6,13 +6,32 @@ import android.os.Looper;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Weather weather;
+
 @Override
 protected void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    weather = new Weather(); // TODO - only for me to make the later code, do this properly later please
+
     // todo: to pass into WeatherApi what to update
+
+    // Find the RecyclerView and set it's adaptor to the RecyclerViewAdaptor
+
+    // Finding RecyclerViews
+    RecyclerView todayRecyclerView = (RecyclerView) findViewById(R.id.todayRecycler);
+
+    // Creating the adaptor instances
+    RecyclerViewAdaptor todayAdaptor = new RecyclerViewAdaptor(weather);
+
+    // Setting RecyclerView adaptors
+    todayRecyclerView.setAdapter(todayAdaptor);
+
 }
 }
--- app/src/main/java/id/weather/RecyclerViewAdaptor.java
+++ app/src/main/java/id/weather/RecyclerViewAdaptor.java
@@ -1,5 +1,7 @@
 package id.weather;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -7,20 +9,41 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdaptor extends RecyclerView.Adapter<RecyclerViewViewHolder> {
 
+    private static final int itemCount = 6;
+
+    private final Weather weather;
+
+    public RecyclerViewAdaptor(Weather weather) {
+
+        this.weather = weather;
+
+    }
+
     @NonNull
     @Override
     public RecyclerViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null; // TODO
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.weather_row_item, parent, false);
+
+        RecyclerViewViewHolder holder = new RecyclerViewViewHolder(v, 0);
+
+        weather.attach(holder);
+
+        return holder;
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewViewHolder holder, int position) {
-        // TODO
+
+        holder.setIndex(position);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0; // TODO
+        return itemCount;
     }
 
 }

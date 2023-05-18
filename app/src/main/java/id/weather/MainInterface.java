54362e01c942789b25f84ee7242646f898914d2b
Robert Woodland <robertwoodland47@gmail.com>
2023-05-18 18:34:45 +0100

    Added parameters for mainInterface

--- app/src/main/java/id/weather/MainInterface.java
+++ app/src/main/java/id/weather/MainInterface.java
@@ -2,13 +2,19 @@ package id.weather;
 
 import android.os.Handler;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainInterface implements IObserver {
     private final ImageView img;
+    private final TextView currentTempTxt;
+    private final TextView currentWeatherTxt;
     private final Handler handler;
 
-    public MainInterface(ImageView img, Handler handler) {
+    public MainInterface(ImageView img, TextView currentTempTxt, TextView currentWeatherTxt, Handler handler) {
         this.img = img;
+        this.currentTempTxt = currentTempTxt;
+        this.currentWeatherTxt = currentWeatherTxt;
+
         this.handler = handler;
     }
 
@@ -16,8 +22,11 @@ public class MainInterface implements IObserver {
     public void update(WeatherData weatherData) {
         this.handler.post(() -> {
             if (weatherData.getIsSuccess()) {
-                int currentWeather = weatherData.getDaily().getWeathercode(0);
+                int currentTime = weatherData.getCurrentHour();
+
+                int currentWeather = weatherData.getHourly().getWeathercode(0);
                 img.setImageResource(Weathercode.toIcon(currentWeather, 1));
+                currentWeatherTxt.setText(Weathercode.toText(currentWeather));
                 // if hourly: weatherData.getHourly().getIs_day(...)
             }
         });

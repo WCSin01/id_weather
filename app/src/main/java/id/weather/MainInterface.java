e6ae10128e54e38627df88692ba1751e74bd686f
Robert Woodland <robertwoodland47@gmail.com>
2023-05-19 18:15:49 +0100

    Corrected capitalisation on user facing text

--- app/src/main/java/id/weather/MainInterface.java
+++ app/src/main/java/id/weather/MainInterface.java
@@ -25,13 +25,14 @@ public class MainInterface implements IObserver {
     }
 
     private int nextPrecipTime(WeatherData weatherData) {
-        int t = 24; // placeholder out of range
-        for (int i = 23; i > (weatherData.getCurrentHour() - 1); i--) {
-            if (weatherData.getHourly().getPrecipitation_probability(i) >= 0.25f) {
+        int currentTime = weatherData.getCurrentHour();
+        int t = weatherData.getCurrentHour() + 24; // placeholder out of range
+        for (int i = currentTime + 23; i > (currentTime - 1); i--) {
+            if (weatherData.getHourly().getPrecipitation_probability(i) >= 25f) {
                 t = i;
             }
         }
-        return t; // Note: a returned value of 24 means no rain in next 24hrs
+        return t; // Note: a returned value of 24 would mean no rain today
     }
 
     @Override
@@ -49,7 +50,16 @@ public class MainInterface implements IObserver {
                 currentTempTxt.setText(String.format("%s°C", weatherData.getHourly().getTemperature_2m(currentTime)));
                 highTempTxt.setText(String.format("H: %s°C", weatherData.getDaily().getTemperature_2m_max(0)));
                 lowTempTxt.setText(String.format("L: %s°C", weatherData.getDaily().getTemperature_2m_min(0)));
-                rainChanceTxt.setText(String.format("%s%% chance of rain at %s:00", Math.round(weatherData.getHourly().getPrecipitation_probability(nextPrecip)), nextPrecip));
+
+                if (nextPrecip < currentTime + 24) {
+                    int nextTime = nextPrecip;
+                    if (nextTime >= 24) {
+                        nextTime -= 24;
+                    }
+                    rainChanceTxt.setText(String.format("%s%% chance of rain at %s:00", Math.round(weatherData.getHourly().getPrecipitation_probability(nextPrecip)), nextTime));
+                } else {
+                    rainChanceTxt.setText("No rain expected soon!");
+                }
                 // if hourly: weatherData.getHourly().getIs_day(...)
             }
         });
